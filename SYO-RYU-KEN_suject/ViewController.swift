e74e141a6c8b09c8fb534883fe93038695fca021
dmiyakoshi <d1m354dai@gmail.com>
2021-11-03 14:03:26 +0900

    新規作成 for文のスタートを1から始めるように変更しました。

--- SYO-RYU-KEN_suject/ViewController.swift
+++ SYO-RYU-KEN_suject/ViewController.swift
@@ -9,11 +9,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    func displayImage() {
+            let image = UIImage(named: "attak1")
+            imageView.image = image
+        }
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        displayImage()
     }
 
-
+    @IBAction func button(_ sender: Any) {
+        var imageListArray :Array<UIImage> = []
+        
+        for number in 1...20 {
+            imageListArray.append( UIImage(named: "attak\(number)")!)
+        }
+        
+        imageView.animationImages = imageListArray
+        imageView.animationDuration = 3
+        imageView.animationRepeatCount = 1
+        
+        imageView.startAnimating()
+        
+    }
 }
 
