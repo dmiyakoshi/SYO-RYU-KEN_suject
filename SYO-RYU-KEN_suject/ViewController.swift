2283800b0f80d32059238c6f1ae1737e18edfa7e
dmiyakoshi <d1m354dai@gmail.com>
2021-11-03 13:40:33 +0900

    Initial Commit

---
+++ SYO-RYU-KEN_suject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SYO-RYU-KEN_suject
+//
+//  Created by 宮越大輝 on 2021/11/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
